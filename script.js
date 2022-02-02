92a4ee729c788268a12a0e3ebc8ee45079d899bf
Andrew Killian <ack97@txstate.edu>
2022-02-01 18:51:42 -0600

    get element by id returning null

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-forgot-password.addEventListener('click', () => {
+console.log(document.getElementById("forgot-password"));
+document.getElementById("reset-password").addEventListener('click', () => {
+    console.log('no');
     document.querySelector("main").style.display = 'none';
-    document.getElementById("forgot-password") = 'flex';
+    document.getElementById("forgot-password").style.display = 'flex';
 })
