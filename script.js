6a920d4ce4db6b6e9fd087f0b6955ecc40779a15
Andrew Killian <ack97@txstate.edu>
2022-02-01 19:36:21 -0600

    linked third aside form

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById("reset").addEventListener('click', () => {
     document.querySelector("main").style.display = 'none';
     document.getElementById("forgot-password").style.display = 'flex';
 })
+
+document.getElementById("create").addEventListener('click', () => {
+    document.querySelector("main").style.display = 'none';
+    document.getElementById("create-account").style.display = 'initial';
+})
