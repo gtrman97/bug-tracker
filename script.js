79378adef6b1a376e84688e5bc1e6cb83601865a
Andrew Killian <ack97@txstate.edu>
2022-02-01 19:16:52 -0600

    deleted line

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log(document.getElementById("reset-password"));
 document.getElementById("reset-password").addEventListener('click', () => {
     document.querySelector("main").style.display = 'none';
     document.getElementById("forgot-password").style.display = 'flex';
