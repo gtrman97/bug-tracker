53ffaf88a3da84458f9e8754a3d38af6431ec55f
Andrew Killian <ack97@txstate.edu>
2022-03-26 16:33:31 -0500

    did stuff

--- site/demo-pages/demo-server/index.js
+++ site/demo-pages/demo-server/index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const pool = require('./db');
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello from Bug Tracker Express");
+})
+
+app.listen(port, () => {
+    console.log(`bug tracker server has started on port ${port}`);
+})
