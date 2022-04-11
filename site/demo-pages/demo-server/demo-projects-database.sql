dbbeaa92f32447976f9d67c9e7978e7a5529d64a
Andrew Killian <ack97@txstate.edu>
2022-04-11 18:38:58 -0500

    changed table data type

--- site/demo-pages/demo-server/demo-projects-database.sql
+++ site/demo-pages/demo-server/demo-projects-database.sql
@@ -1,7 +1,7 @@
 create table demo_projects (
     id BIGSERIAL NOT NULL PRIMARY KEY, 
     project_name VARCHAR(50) NOT NULL, 
-    project_manager_id VARCHAR(50) NOT NULL,
+    project_manager_id BIGSERIAL NOT NULL,
     project_status VARCHAR(25) NOT NULL
 );
 INSERT INTO demo_projects (project_name, project_manager_id, project_status) VALUES ('Project 1', 4, 'In Progress');
