a48463633fd9ffecb5d72ce02c11568cebff2d63
Andrew Killian <ack97@txstate.edu>
2022-04-12 14:28:58 -0500

    changed file

--- site/demo-pages/demo-server/demo-users-database.sql
+++ site/demo-pages/demo-server/demo-users-database.sql
@@ -2,15 +2,15 @@ create table demo_users (
     id BIGSERIAL NOT NULL PRIMARY KEY, 
     full_name VARCHAR(50) NOT NULL, 
     email VARCHAR(50) NOT NULL,
-    title VARCHAR(25) NOT NULL
+    role_id INTEGER
 );
-INSERT INTO demo_users (full_name, email, title) VALUES ('John Michael', 'john@john-michael.com', 'Developer');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Alexa Liras', 'alexa@alexa-liras.com', 'Submitter');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Lauren Perrier', 'lauren@lauren-perrier.com', 'Submitter');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Michael Levi', 'michael@michael-levi.com', 'Project Manager');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Richard Gran', 'richard@richard-gran.com', 'Project Manager');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Miriam Eric', 'miriam@miriam-eric.com', 'N/A');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Jimmy Jones', 'jimmy@jimmy-jones.com', 'Developer');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Trevor Strnad', 'trevor@trevor-strnad.com', 'Project Manager');
-INSERT INTO demo_users (full_name, email, title) VALUES ('Brian Eschbach', 'brian@brian-eschbach.com', 'Project Manager');
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('John Michael', 'john@john-michael.com', 3);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Alexa Liras', 'alexa@alexa-liras.com', 4);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Lauren Perrier', 'lauren@lauren-perrier.com', 4);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Michael Levi', 'michael@michael-levi.com', 2);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Richard Gran', 'richard@richard-gran.com', 2);
+INSERT INTO demo_users (full_name, email) VALUES ('Miriam Eric', 'miriam@miriam-eric.com');
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Jimmy Jones', 'jimmy@jimmy-jones.com', 3);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Trevor Strnad', 'trevor@trevor-strnad.com', 2);
+INSERT INTO demo_users (full_name, email, role_id) VALUES ('Brian Eschbach', 'brian@brian-eschbach.com', 2);
 
