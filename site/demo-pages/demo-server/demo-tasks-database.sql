68e668ebb190235146958eacc73076c52b3ff4e4
Andrew Killian <ack97@txstate.edu>
2022-04-11 20:46:43 -0500

    added database file

--- site/demo-pages/demo-server/demo-tasks-database.sql
+++ site/demo-pages/demo-server/demo-tasks-database.sql
@@ -1,7 +1,9 @@
 create table demo_tasks (
     id BIGSERIAL NOT NULL PRIMARY KEY, 
     task_name VARCHAR(50) NOT NULL, 
-    project_id BIGSERIAL NOT NULL,
-    developer_id BIGSERIAL NOT NULL,
-    task_status VARCHAR(25) NOT NULL
+    project_id INTEGER NOT NULL,
+    developer_id INTEGER,
+    task_status VARCHAR(25) NOT NULL DEFAULT('Not Started')
 );
+
+INSERT INTO demo_tasks (task_name, project_id) VALUES ('Task 1', 1);
