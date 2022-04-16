a1ed53c3367bd4225efd53bec11345c0de68cb8e
Andrew Killian <ack97@txstate.edu>
2022-04-16 17:21:32 -0500

    possibly made progress

--- site/demo-pages/demo-app/src/demo-components/StatusColumn.js
+++ site/demo-pages/demo-app/src/demo-components/StatusColumn.js
@@ -1,9 +1,9 @@
-import "../../../../assets/css/nucleo-icons.css";
-import "../../../../assets/css/nucleo-svg.css";
-import "../../../../assets/css/material-dashboard.css?v=3.0.0";
+import '../bootstrap-css/nucleo-icons.css';
+import '../bootstrap-css/nucleo-svg.css';
+import "../bootstrap-css/material-dashboard.css?v=3.0.0";
 import "./StatusColumn.css";
-import "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700";
-import "https://fonts.googleapis.com/icon?family=Material+Icons+Round";
+// import "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700";
+// import "https://fonts.googleapis.com/icon?family=Material+Icons+Round";
 
 function StatusColumn() {
   return (
