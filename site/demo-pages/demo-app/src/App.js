9397314d5384266af9a59d7aa26be30aa89792f5
Andrew Killian <ack97@txstate.edu>
2022-04-16 23:43:37 -0500

    added button

--- site/demo-pages/demo-app/src/App.js
+++ site/demo-pages/demo-app/src/App.js
@@ -1,12 +1,20 @@
 import StatusColumn from "./demo-components/StatusColumn";
 
 function App() {
+
+  let createTicket = () => {
+    alert('New Ticket!');
+  }
+
   return (
+    <div>
+      <button onClick={createTicket}>Create Ticket</button>
     <div style={{display: "flex", gap: "2%", padding: "2.5%", margin: "5%", border: "solid 2px black"}}>
       <StatusColumn status={'not started'} color={'primary'}/>
       <StatusColumn status={'in progress'} color={'secondary'}/>
       <StatusColumn status={'completed'} color={'success'}/>
       
+    </div>
     </div>
   );
 }
