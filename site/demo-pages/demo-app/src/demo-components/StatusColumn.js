91b6dad6bcb28d80e3b171813ee4c951c8e8cb7b
Andrew Killian <ack97@txstate.edu>
2022-04-16 21:53:06 -0500

    added tickets

--- site/demo-pages/demo-app/src/demo-components/StatusColumn.js
+++ site/demo-pages/demo-app/src/demo-components/StatusColumn.js
@@ -38,139 +38,15 @@ function StatusColumn(props) {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td className="align-middle text-center">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-center text-xs mb-0 text-secondary">Task 1</h6>
-                        </div>
-                    </td>
-                    <td className="text-center">
-                      <p className="text-xs text-secondary mb-0">John Michael</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">1h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 2</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">John Michael</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">2h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 3</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Jimmy Jones</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">1h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 4</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Trevor Strnad</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">3h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 5</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Brian Eschbach</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">4h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 6</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Miriam Eric</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">1h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 7</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Jimmy Jones</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">2h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 8</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Trevor Strnad</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">4h</span>
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <div className="d-flex px-2 py-1">
-                        <div className="d-flex flex-column justify-content-center">
-                          <h6 className="text-xs mb-0 text-secondary">Task 8</h6>
-                        </div>
-                      </div>
-                    </td>
-                    <td>
-                      <p className="text-xs text-secondary mb-0">Brian Eschbach</p>
-                    </td>
-                    <td className="align-middle text-center">
-                      <span className="text-secondary text-xs">3h</span>
-                    </td>
-                  </tr>
+                  <Ticket name={'Task 1'} developer={'John Michael'} time={'1h'}/>
+                  <Ticket name={'Task 2'} developer={'John Michael'} time={'2h'}/>
+                  <Ticket name={'Task 3'} developer={'Jimmy Jones'} time={'1h'}/>
+                  <Ticket name={'Task 4'} developer={'Trevor Strnad'} time={'3h'}/>
+                  <Ticket name={'Task 5'} developer={'Brian Eschbach'} time={'4h'}/>
+                  <Ticket name={'Task 6'} developer={'Miriam Eric'} time={'1h'}/>
+                  <Ticket name={'Task 7'} developer={'Jimmy Jones'} time={'2h'}/>
+                  <Ticket name={'Task 8'} developer={'Trevor Strnad'} time={'4h'}/>
+                  <Ticket name={'Task 9'} developer={'Brian Eschbach'} time={'3h'}/>
                 </tbody>
               </table>
             </div>
